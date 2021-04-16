1190468db8e2d14241a85b579fc844e419056217
arihant jain <jain.ari2000@gmail.com>
2021-04-16 20:32:05 +0530

    changed text color on clicked

--- src/index.js
+++ src/index.js
@@ -4,18 +4,11 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { BrowserRouter} from "react-router-dom";
 import store from './redux/store';
 
 ReactDOM.render(
     <Provider store={store}>
-      <BrowserRouter>
       <App />
-      </BrowserRouter>
     </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
