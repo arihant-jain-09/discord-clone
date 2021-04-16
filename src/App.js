51d556d09372dfae8d5dbc26f0792be7fcd99b75
arihant jain <jain.ari2000@gmail.com>
2021-04-16 20:25:12 +0530

    removed routing made a single page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React from 'react';
-import { Route,Redirect,Switch } from 'react-router-dom';
-import './App.css';
-import Homepage from './components/homepage/homepage.jsx'
+import './App.scss'
 import Main from './components/Main/Main.jsx'
 import {auth} from './firebase/firebase'
 import {useAuthState} from 'react-firebase-hooks/auth'
-
+import firebase from 'firebase/app'
+import { Button } from '@material-ui/core';
 
 function App() {
   const [user]=useAuthState(auth);
+  const signInWithGoogle=()=>{
+    const provider=new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+}
   return (
     <div className="App">
       <header className="App-header">
         
       </header>
       <main>
-        
-        <Switch>
-          {console.log(user)}
-          <Route exact path='/' render={()=>user?(<Redirect to='/channels'/>):<Homepage/>} />
+         {
+          user? 
+          <Main user={user}/>
+          : (<div className="discord__homepage">
+             <img className="discord__homepage-svg" src="./discord.svg" alt="Discord Svg"/>
+            <Button onClick={signInWithGoogle} className="discord__homepage-btn" color='primary' variant='contained'>Sign In</Button>
+            </div>)
+        }
+          {/* <Route exact path='/' render={()=>user?(<Redirect to='/channels'/>):<Homepage/>} />
           <Route exact path='/channels' render={(user) => ( <Main user={user} isAuthed={true} />
             )}/>
-        </Switch>
+        </Switch> */}
       </main>
     </div>
   );
